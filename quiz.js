193769824a45f4f164dc63763a92e4347a241401
James Smith <james@Jamess-Mac-mini.gateway>
2023-04-17 03:38:29 +0800

    04-Quiz-html_css_js-createHighScores

--- quiz.js
+++ quiz.js
@@ -1,19 +1,39 @@
+// Targeting question declared as a constant using querySelector() method returning the first element in the document with the question ID
 const question = document.querySelector('#question');
+
+// Targeting choices declared as a constant using querySelectorAll() method returning an array from a static list through the Array.from() method
 const choices = Array.from(document.querySelectorAll('.choice-text'));
+
+// Targeting timerID declared as a constant using querySelector() method returning the first element in the document with the score ID
 const timerID = document.querySelector('#score');
 
-
+// Declaring curentQuestion as an empty object
 let currentQuestion = {};
+
+// Declaring acceptingAnswers (correct answer) as a Booleans true
 let acceptingAnswers = true;
+
+// Declaring the starting timer count down value at 1 minute
 let score = 60;
+
+// Declaring availableQuestions as an empty array
 let availableQuestions = [];
+
+// Create variable questions and let the questions be an array to store multiple question(s) in a single variable named questions
 let questions =[
   {
+    // Let the question be an object with the following strings
+    // String '0'
     question: 'Commonly used data types DO Not include:',
+    // String '1'
     choice1: 'strings',
+    // String '2'
     choice2: 'booleans',
+    // String '3'
     choice3: 'alerts',
+    // String '4'
     choice4: 'numbers',
+    // String '5' indicating the answer to this question is string '3'
     answer: 3,
   },
   {
@@ -48,39 +68,52 @@ let questions =[
     choice4: 'console.log',
     answer: 4,
   },
-]
+];
 
 
+// Creating the countDownTimer function
 function countDownTimer(){
-  let timerId = setInterval(function(){
+  // Creating a timer that calls the countDownTimer function every second (defined below)
+  let timer = setInterval(function(){
+    // The -- decrement operator decreases the time at a defined interval below
     score--;
+    // Display the content of the element(s) including the script and the style through the textContent property which will be the score
     timerID.textContent = score;
 
+    // When the timer runs out (less than or equal to 0 seconds left)
     if(score <= 0) {
-      clearInterval(timerId);
-      alert("Quiz Over -- The End !!! Run through the rest of the remaining question at your leisure for practice but your final score will not be recorded as you have not completed the quiz in the allocated time.")
-    }
+      // clearInterval method cancels the the repeating action of the 'timer'
+      clearInterval(timer);
+      // alert method commands the browser to display Quiz Over message as the quiz ended
+      alert("Quiz Over -- The End !!! Run through the rest of the remaining question at your leisure for practice but your final score will not be recorded as you have not completed the quiz in the allocated time.");
+    };
+  
+  // Define the setInterval Interval at 1 second
   }, 1000);
-}
+};
 
 
+// Creating the startQuiz function to initiate quiz
 function startQuiz() {
+  // Calling the countDownTimer function to commence counting down
   countDownTimer();
+  // Determine the number of total available questions
   availableQuestions = [...questions]
+  // Calling the getNewQuestion function to retrive a new question
   getNewQuestion()
 }
 
-
+// Creating the getNewQuestion function to retrive a new question and display the currentQuestion with the choices available
 function getNewQuestion() {
-
+  // When there is no more new questions available launch the End Page using the assign static method
   if(availableQuestions.length === 0) {
     return window.location.assign('./end.html')
   }
-
+  // Selecting the array of question randomly using the Math.floor and Math.ramdom displaying the currentQuestion content through innerText
   const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
   currentQuestion = availableQuestions[questionsIndex]
   question.innerText = currentQuestion.question
-
+  // Using the forEach Method to extract the text content of multiple choice 1-4 read by the dataset fromm the HTML element in relation the respective currentQuestion displaying using innerText
   choices.forEach(function(choice) {
     const number = choice.dataset['number']
     choice.innerText = currentQuestion['choice' + number]
@@ -113,7 +146,7 @@ function getNewQuestion() {
         selectedChoice.parentElement.classList.remove(classToApply)
         getNewQuestion()
       
-        }, 1000)
+        }, 100)
     })
   })
 
